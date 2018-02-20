9d76dd6c6cc00ce386789dfb3c075c4fe45d78f9
Draco <playngdragon@gmail.com>
2018-02-20 22:20:50 +0900

    テストの第一歩を踏み出す。　テストの書き方はこれから。

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 fn main() {
     let count:i32 = 100;
 
-    fizzbuzz(count);
+    for italater in 0..count {
+    fizzbuzz(italater);
+    }
 }
 
 fn fizzbuzz(italate_counter: i32){    
-    for italater in 0..italate_counter {
-        if italater % 3 == 0 && italater % 5 == 0 {
-            println!("{} fizzbuzz",italater);
-        }
-        else if italater % 3 == 0 {
-            println!("{} fizz",italater);
-        }
-        else if italater % 5 == 0 {
-            println!("{} buzz",italater);
-        }
-        else{
-            println!("{}",italater);
-        }
+    if italate_counter % 3 == 0 && italate_counter % 5 == 0 {
+        println!("{} fizzbuzz",italate_counter);
     }
+    else if italate_counter % 3 == 0 {
+        println!("{} fizz",italate_counter);
+    }
+    else if italate_counter % 5 == 0 {
+        println!("{} buzz",italate_counter);
+    }
+    else{
+        println!("{}",italate_counter);
+    }
+}
+
+#[test]
+fn can_test(){
+    assert_eq!(1,1);
 }
