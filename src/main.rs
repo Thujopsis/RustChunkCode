aced96e3668567d86202d030558386e83851f265
Draco <playngdragon@gmail.com>
2018-02-21 20:52:14 +0900

    パターンマッチ版とテスト追加。　これでFizzbuzzはおしまい。

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ fn main() {
     let count:i32 = 100;
 
     for italater in 0..count {
-    println!("{}",fizzbuzz(italater));
+    //println!("{}",fizzbuzz(italater));
+    println!("{}",pattern_fizzbuzz(italater));
     }
 }
 
@@ -18,6 +19,15 @@ fn fizzbuzz(italate_counter: i32) -> String{
     }
     else{
         italate_counter.to_string()
+    }
+}
+
+fn pattern_fizzbuzz(italate_counter :i32) -> String{
+    match (italate_counter % 3, italate_counter % 5) {
+        (0,0) => "fizzbuzz".to_string(),
+        (0,_) => "fizz".to_string(),
+        (_,0) => "buzz".to_string(),
+        (_,_) => italate_counter.to_string(),
     }
 }
 
@@ -39,4 +49,19 @@ fn fizzbuzz_divided5(){
 #[test]
 fn fizzbuzz_divided15(){
     assert_eq!(fizzbuzz(15),"fizzbuzz".to_string());
+}
+
+#[test]
+fn pattern_fizzbuzz_divided3(){
+    assert_eq!(pattern_fizzbuzz(3),"fizz".to_string());
+}
+
+#[test]
+fn pattern_fizzbuzz_divided5(){
+    assert_eq!(pattern_fizzbuzz(5),"buzz".to_string());
+}
+
+#[test]
+fn pattern_fizzbuzz_divided15(){
+    assert_eq!(pattern_fizzbuzz(15),"fizzbuzz".to_string());
 }
